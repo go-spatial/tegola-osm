e58c0be36cbdfe850fb38e21b4ae839b2cd918a1
Alexander Rolek <a.rolek@gmail.com>
2017-03-29 13:00:15 -0700

    DB connection vars for natural earth import script

--- naturalearth.sh
+++ naturalearth.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+# This script will install natural earth data (http://www.naturalearthdata.com/downloads/) into a PostGIS database name DB_NAME.
+# The script assumes the following utilities are installed:
+# 	- psql: PostgreSQL client
+#	- ogr2ogr: GDAL vector lib
+#	- unzip: decompression util
+#
+# Usage
+# 	Set the database connection variables, then run
+#
+#		./naturalearth.sh
+#
+# Important
+# 	- This script is idempotent and will DROP the natural earth database if it already exists
+#	- In order for this script to work the DB_USER must have access to the 'postgres' database to create a new database
+
+
 set -e
 
+# database connection variables
 DB_NAME="natural_earth"
+DB_HOST=""
+DB_PORT=""
+DB_USER=""
+DB_PW=""
 
 # array of natural earth dataset URLs
  dataurls=(
@@ -16,9 +37,9 @@ DB_NAME="natural_earth"
 	"http://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_0_boundary_lines_disputed_areas.zip"
 )
 
-psql postgres -c "DROP DATABASE IF EXISTS $DB_NAME"
-psql postgres -c "CREATE DATABASE $DB_NAME"
-psql $DB_NAME -c "CREATE EXTENSION postgis"
+psql "dbname='postgres' host='$DB_HOST' port='$DB_PORT' user='$DB_USER' password='$DB_PW'" -c "DROP DATABASE IF EXISTS $DB_NAME"
+psql "dbname='postgres' host='$DB_HOST' port='$DB_PORT' user='$DB_USER' password='$DB_PW'" -c "CREATE DATABASE $DB_NAME"
+psql "dbname='$DB_NAME' host='$DB_HOST' port='$DB_PORT' user='$DB_USER' password='$DB_PW'" -c "CREATE EXTENSION postgis"
 
 # iterate our dataurls
 for i in "${!dataurls[@]}"; do
@@ -29,7 +50,7 @@ for i in "${!dataurls[@]}"; do
 	unzip $i -d $i
 
 	# reproject data to webmercator (3857) and insert into our database
-	OGR_ENABLE_PARTIAL_REPROJECTION=true ogr2ogr -t_srs EPSG:3857 -nlt PROMOTE_TO_MULTI -f PostgreSQL PG:dbname=$DB_NAME $i/*.shp
+	OGR_ENABLE_PARTIAL_REPROJECTION=true ogr2ogr -t_srs EPSG:3857 -nlt PROMOTE_TO_MULTI -f PostgreSQL PG:"dbname='$DB_NAME' host='$DB_HOST' port='$DB_PORT' user='$DB_USER' password='$DB_PW'" $i/*.shp
 
 	# clean up
 	rm -rf $i/ $i.zip
