d8da37f242677e32a3f0c3e7e27f97f81b642f9f
Alexander Rolek <a.rolek@gmail.com>
2017-07-19 10:01:42 -0700

    converted img, css, js references to relative paths. added "use strict" to the main.js. moved accessTokens and various other params to a global config. added numerous TODO comments

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,40 +1,58 @@
+'use strict'
+
+var config = {
+  mapboxAccessToken: 'pk.eyJ1IjoiamowaG5zMG4iLCJhIjoiY2o1YjRtMjZpMGd2MDJ3bW00bnA5NXdyMiJ9.qlR4a_qfTlKZs1Qisk6sAg',
+  startingCenter: [-117.15117725013909, 32.72269876352742],
+  startingZoom: 15
+};
+
 var map, currentLib, currentStyle;
+
+//  TODO: init this later?
 var openLayersSatellite = new ol.layer.Tile({
-            title: 'DigitalGlobe Maps API: Recent Imagery',
-            source: new ol.source.XYZ({
-                                url: 'http://api.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNqNWIyMHkxdzBmNGczNG55bGNhc2tlcncifQ.lQXsl-GCFgJWmIKEeaRpPg',
-            attribution: "© DigitalGlobe, Inc"
-                            })})
+  title: 'DigitalGlobe Maps API: Recent Imagery',
+  source: new ol.source.XYZ({
+    url: 'http://api.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.png?access_token='+config.mapboxAccessToken,
+    attribution: "© DigitalGlobe, Inc"
+  })
+});
+
 var openlayers = {
   init: function() {
-    map = olms.apply('map', '/styles/'+currentStyle+'.json');
-    map.getView().setCenter(ol.proj.fromLonLat([-117.15117725013909, 32.72269876352742]));
-    map.getView().setZoom(15);
+    //  TODO: use the switchStyle method?
+    map = olms.apply('map', 'styles/'+currentStyle+'.json');
+    map.getView().setCenter(ol.proj.fromLonLat(config.startingCenter));
+    map.getView().setZoom(config.startingZoom);
   },
   switchStyle: function(style) {
     map.getLayers().getArray().forEach(function(layer) {
       map.removeLayer(layer);
     });
-    map = olms.apply(map, '/styles/'+style+'.json');
+    map = olms.apply(map, 'styles/'+style+'.json');
+    //  TODO: move this to a gloabl config that maps the style names with style.json files and satellite basemaps if necessary.
+    //  bonus: build the style buttons from the config on app init
     if(style === 'night-vision3d') {
       map.addLayer(openLayersSatellite);
     }
   }
 }
-mapboxgl.accessToken = 'pk.eyJ1IjoiamowaG5zMG4iLCJhIjoiY2o1YjRtMjZpMGd2MDJ3bW00bnA5NXdyMiJ9.qlR4a_qfTlKZs1Qisk6sAg';
+
+//  TODO: move to the mapbox.init() method?
+mapboxgl.accessToken = config.mapboxAccessToken;
 var mapbox = {
   init: function() {
     map = new mapboxgl.Map({
       container: 'map', // container id
-      style: '/styles/'+currentStyle+'.json',
-      center: [-117.15117725013909, 32.72269876352742], 
-      zoom: 15,
+      style: 'styles/'+currentStyle+'.json',
+      center: config.startingCenter, 
+      zoom: config.startingZoom,
       hash: true 
     });
     map.addControl(new mapboxgl.NavigationControl());
   },
   switchStyle: function(style) {
-    map.setStyle('/styles/'+style+'.json');
+    map.setStyle('styles/'+style+'.json');
+    //  TODO: move this to a gloabl config that maps the style names with style.json files and satellite basemaps if necessary.
     if(style === 'night-vision3d') {
     }
   }
@@ -50,4 +68,5 @@ var switchStyle = function(style) {
 }
 currentLib = mapbox;
 currentStyle = 'mapbox';
+//  TODO: it appears you're using an "interface" strategy, so wouldn't it be appropriate to call currentLib.init()?
 mapbox.init();
